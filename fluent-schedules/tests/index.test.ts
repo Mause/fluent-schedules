870af09fb3f9d320c836e4faad58b8d13ce6b5e1
Elliana May <me@mause.me>
2026-03-27 01:08:35 +0800

    base case

--- fluent-schedules/tests/index.test.ts
+++ fluent-schedules/tests/index.test.ts
@@ -1,5 +1,6 @@
-import { parse } from "fluent-schedules";
 import { expect, test } from "vitest";
+
+import { iterate, parse } from "../src/index.ts";
 
 test.for([
   "third thursday of the month",
@@ -7,4 +8,10 @@ test.for([
   "last thursday of the month at six thirty",
 ])("parse(%s)", (param) => {
   expect(parse(param)).toMatchSnapshot();
+});
+
+test("iterate", () => {
+  expect(
+    iterate(new Date(2026, 1, 1), "third tuesday of the month", {}),
+  ).toMatchSnapshot();
 });
