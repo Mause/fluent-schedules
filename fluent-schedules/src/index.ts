f4e99b414696fb4d7691ef0114d4ce893d17076b
Elliana May <me@mause.me>
2026-03-26 23:46:21 +0800

    build the outline of a library

--- fluent-schedules/src/index.ts
+++ fluent-schedules/src/index.ts
@@ -1,1 +1,58 @@
-export { parse } from "./grammar.pegjs";
+import {
+  addDays,
+  addWeeks,
+  type Day,
+  eachDayOfInterval,
+  formatDate,
+  getDay,
+  type Locale,
+  nextDay,
+  startOfISOWeek,
+} from "date-fns";
+
+import { parse } from "./grammar.js";
+
+function getNthDay(firstOfMonth: Date, n: number, day: Day): Date {
+  // If the 1st is already Thursday, it's the 1st Thursday.
+  // Otherwise, get the next Thursday.
+  const firstThursday =
+    getDay(firstOfMonth) === day ? firstOfMonth : nextDay(firstOfMonth, day);
+
+  // Add 2 weeks to get from the 1st to the 3rd Thursday
+  return addWeeks(firstThursday, n - 1);
+}
+
+function parseDay(day: string, { locale }: { locale: Locale }): Day {
+  const today = startOfISOWeek(new Date());
+  const daysOfWeek = eachDayOfInterval({
+    start: today,
+    end: addDays(today, 6),
+  });
+
+  const days = daysOfWeek.map((date) =>
+    formatDate(date, "EEEE", {
+      locale,
+    }),
+  );
+
+  return days.indexOf(day) as Day;
+}
+
+export function iterate(
+  referenceDate: Date,
+  input: string,
+  { locale }: { locale: Locale },
+) {
+  const parsed = parse(input) as {
+    index: 3;
+    day_of_week: "tuesday";
+    trailer: "of the month";
+    time: null;
+  };
+
+  return getNthDay(
+    referenceDate,
+    parsed.index,
+    parseDay(parsed.day_of_week, { locale }),
+  );
+}
